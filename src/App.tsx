d844953de4edd9eba221b1189803b70638565b95
Guilherme <guilherme.1009@hotmail.com>
2024-03-21 20:43:43 -0300

    Create component Catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 
-import ProductDetails from "./routes/ProductDetails";
+import Catalog from "./routes/Catalog";
+
 
 
 export default function App() {
   return (
-      <ProductDetails/>
+      <Catalog/>
   );
 }
 
