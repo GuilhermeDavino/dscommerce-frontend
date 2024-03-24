d7fefb1356c33da994c02b0cf448c736b00715db
Guilherme <guilherme.1009@hotmail.com>
2024-03-24 16:50:52 -0300

    Adding routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Catalog from "./routes/Catalog/index.tsx";
+import ProductDetails from "./routes/ProductDetails/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="Catalog" element={<Catalog />}/>
+        <Route path="ProductDetails" element={<ProductDetails />}/>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
