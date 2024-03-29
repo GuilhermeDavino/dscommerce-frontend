9364a438217963d5bdc5e836ad350b93efc160da
Guilherme <guilherme.1009@hotmail.com>
2024-03-29 10:01:58 -0300

    Link new Product

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -8,6 +8,8 @@ import SearchBar from "../../../components/SearchBar";
 import ButtonNextPage from "../../../components/ButtonNextPage";
 import DialogInfo from "../../../components/DialogInfo";
 import DialogConfirmation from "../../../components/DialogConfirmation";
+import ButtonInverse from "../../../components/ButtonInverse";
+import { useNavigate } from "react-router-dom";
 
 type QueryParams = {
   page: number;
@@ -16,7 +18,9 @@ type QueryParams = {
 
 export default function ProductListing() {
   
-    const [dialogConfirmationData, setDialogConfirmationData] = useState({
+  const navigate = useNavigate();
+
+  const [dialogConfirmationData, setDialogConfirmationData] = useState({
         visible: false,
         id: 0,
         message: "Tem certeza?"
@@ -46,6 +50,10 @@ export default function ProductListing() {
     );
   }, [queryParams]);
 
+  function handleNewProductClick() {
+      navigate("/admin/products/create");
+  }
+  
   function handleSearch(searchText: string) {
     setProducts([]);
     setQueryParams({ ...queryParams, page: 0, name: searchText });
@@ -84,7 +92,10 @@ export default function ProductListing() {
         <h2 className="dsc-section-title dsc-mb20">Cadastro de produtos</h2>
 
         <div className="dsc-btn-page-container dsc-mb20">
-          <div className="dsc-btn dsc-btn-white">Novo</div>
+          <div onClick={handleNewProductClick}>
+            <ButtonInverse text="novo"/>
+          </div>
+          
         </div>
 
         <SearchBar onSearch={handleSearch} />
